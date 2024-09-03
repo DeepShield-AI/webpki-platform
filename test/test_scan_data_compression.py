5622484687d715dccd7f35b3abb3b770a9e124ed
Tianyu Zhang <tianyuz.19@intl.zju.edu.cn>
2024-09-04 00:06:33 +0800

    add ct data compression utils

---
+++ test/test_scan_data_compression.py
@@ -0,0 +1,12 @@
+
+import sys
+sys.path.append(r"D:\global_ca_monitor")
+
+from app.utils.ct_scan_data_compression import CompressScanContent
+
+# compression = CompressScanContent(
+#     load_dir=r'H:/yeti2024',
+#     save_dir=r'H:/yeti2024_compressed'
+# )
+compression = CompressScanContent()
+# compression.start()
