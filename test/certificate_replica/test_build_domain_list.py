fc4bdc7d3cb8b22cd610fd9beeccbd1eab29d830
tianyu <zhangty23@mails.tsinghua.edu.cn>
2024-10-07 00:32:37 +0800

    add scripts to build domain lists from top site associated domains

---
+++ test/certificate_replica/test_build_domain_list.py
@@ -0,0 +1,18 @@
+
+import sys
+sys.path.append(r"D:\global_ca_monitor")
+
+import time
+from app.analyzer.certificate_replicas.build_domain_list import BuildDomainList
+
+# parser = BuildDomainList(
+#     log_name = "sabre",
+#     load_dir = r'D:/global_ca_monitor/data/group_top_domains_sabre',
+#     save_dir = r'D:/global_ca_monitor/data/domain_list'
+# )
+
+parser = BuildDomainList()
+parser.start()
+
+while True:
+    time.sleep(1)
