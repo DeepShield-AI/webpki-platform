88492f8fe3ca0405fbd8b2010bc0cf1d609dea5f
tianyu <zhangty23@mails.tsinghua.edu.cn>
2024-10-07 00:38:17 +0800

    .

--- test/test_scan/test_scan_domain.py
+++ test/test_scan/test_scan_domain.py
@@ -12,14 +12,15 @@ from app.manager.task import TaskBatchTemplate
 with app.app_context():
     scan_type = ScanType(ScanType.SCAN_BY_DOMAIN)
     scan_args = {
-        'MAX_THREADS_ALLOC' : 100,
+        'MAX_THREADS_ALLOC' : 1000,
         'THREAD_WORKLOAD' : 2,
-        'INPUT_DOMAIN_LIST_FILE' : "test_domain_list.csv",
-        'SCAN_PROCESS_NAME': "test 100 domain",
+        'INPUT_DOMAIN_LIST_FILE' : "top10milliondomains.csv",
+        'SCAN_PROCESS_NAME': "test 1-5M domain",
         'STORAGE_DIR' : "out",
         'SCAN_TIMEOUT' : 2,
         'MAX_RETRY' : 2,
-        'NUM_DOMAIN_SCAN' : 100
+        'DOMAIN_RANK_START': 1000000,
+        'NUM_DOMAIN_SCAN' : 5000000
     }
 
     config = DomainScanConfig(**scan_args)
